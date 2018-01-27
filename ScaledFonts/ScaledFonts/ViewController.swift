2c18ddcf93a1a3b9bfb7e8c8d78a826990ee9d43
Mansi Shah <mansi.iosdev@gmail.com>
2018-01-27 20:56:31 +0530

    Added a function to get the scaled version of UIFont

--- ScaledFonts/ScaledFonts/ViewController.swift
+++ ScaledFonts/ScaledFonts/ViewController.swift
@@ -21,28 +21,40 @@ class ViewController: UIViewController {
     }
 
     func setCustomFont() {
-        /// Uncomment the code below to check all the available fonts and have them printed in the console 😉
+        myLabel.font = getScaledFont(forFont: "Cambria-Bold", textStyle: .title1)
+        myLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+    
+    /// Get the Scaled version of your UIFont.
+    ///
+    /// - Parameters:
+    ///   - name: Name of the UIFont whose scaled version you wish to obtain.
+    ///   - textStyle: The text style for your font, i.e Body, Title etc...
+    /// - Returns: The scaled UIFont version with the given textStyle
+    func getScaledFont(forFont name: String, textStyle: UIFontTextStyle) -> UIFont {
+        
+        /// Uncomment the code below to check all the available fonts and have them printed in the console to double check the font name with existing fonts 😉
         
         /*for family: String in UIFont.familyNames
-        {
-            print("\(family)")
-            for names: String in UIFont.fontNames(forFamilyName: family)
-            {
-                print("== \(names)")
-            }
-        }*/
+         {
+         print("\(family)")
+         for names: String in UIFont.fontNames(forFamilyName: family)
+         {
+         print("== \(names)")
+         }
+         }*/
         
-        let userFont =  UIFontDescriptor.preferredFontDescriptor(withTextStyle: .title1)
+        let userFont =  UIFontDescriptor.preferredFontDescriptor(withTextStyle: textStyle)
         let pointSize = userFont.pointSize
-        guard let customFont = UIFont(name: "Cambria-Bold", size: pointSize) else {
+        guard let customFont = UIFont(name: name, size: pointSize) else {
             fatalError("""
-        Failed to load the "Cambria-Bold" font.
+        Failed to load the "\(name)" font.
         Make sure the font file is included in the project and the font name is spelled correctly.
         """
             )
         }
-        myLabel.font = UIFontMetrics.default.scaledFont(for: customFont)
-        myLabel.adjustsFontForContentSizeCategory = true
+        return UIFontMetrics.default.scaledFont(for: customFont)
     }
     
     override func didReceiveMemoryWarning() {
